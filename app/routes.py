efcc6bf93c6a7f338d735d8475b7bbfd67a31204
sock-lobster <dbarter4@gmail.com>
2018-06-28 14:31:08 -0400

    basic pre-travel

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,10 @@
-from flask import render_template
+from flask import render_template, flash, url_for, redirect
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Miguel'}
+    user = {'username': 'Dan'}
     posts = [
         {
             'author': {'username': 'John'},
@@ -16,3 +16,19 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html')
+
+@app.route('/skills')
+def skills():
+    return render_template('skills.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
